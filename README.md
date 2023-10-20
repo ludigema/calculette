b556a497d78deffc62ac56ac5a2790f617942ed9
ludivineUp <44409644+ludivineUp@users.noreply.github.com>
2023-10-20 14:22:21 +0200

    lancement du projet
    main et divide

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # calculette
+
+thomas fonction de l'addition
+wiam fonctiond de la soustraction
+jeremie multiplication
+ludivine division
---
+++ main.py
@@ -0,0 +1,10 @@
+# définir chacun votre fonction
+
+def divide(a,b):
+	res = a/b
+	return res
+
+if __name__ == '__main__':
+	x = 2
+	y = 8
+	print("la division vaut", divide(x,y))
