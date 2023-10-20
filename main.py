ede4ca12b083e7bbed7aa709e9ee0096a1ee39e8
Gamaremy <148548581+Gamaremy@users.noreply.github.com>
2023-10-20 14:36:50 +0200

    Update main.py
    Ajout de la fonctio  multiplication

--- main.py
+++ main.py
@@ -4,7 +4,12 @@ def divide(a,b):
 	res = a/b
 	return res
 
+def multiply(a,b):
+	res=a*b	
+	return res
+
 if __name__ == '__main__':
 	x = 2
 	y = 8
 	print("la division vaut", divide(x,y))
+	print("la multiplication vaut", multiply(x,y))
