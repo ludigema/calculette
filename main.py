cbba0516ffc89301bd8bc1015590c09767b35d82
ludivineUp <44409644+ludivineUp@users.noreply.github.com>
2023-10-20 14:54:41 +0200

    correction diuvide

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 # définir chacun votre fonction
 
 def divide(a,b):
-	res = a/b
+	res = float("NaN")
+	try:
+		res = a / b
+	except Exception as e:
+		print("Pas de division par zéro")
+	
 	return res
 
 if __name__ == '__main__':
 	x = 2
 	y = 8
 	print("la division vaut", divide(x,y))
+	print("la division vaut", divide(x,0))
