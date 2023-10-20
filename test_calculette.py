b83456071aeea0e6ce4af5245119a09590453c71
ludivineUp <44409644+ludivineUp@users.noreply.github.com>
2023-10-20 15:51:09 +0200

    Update test_calculette.py

--- test_calculette.py
+++ test_calculette.py
@@ -2,9 +2,9 @@ import pytest
 import main # nom du fichier
 
 # test unitaire
-def test_div(cal):
+def test_div():
 	assert divide(1,2) == 0.5
 
-def test_raise(cal):
+def test_raise():
 	with pytest.raises(Error):
 		divide(1,0)
